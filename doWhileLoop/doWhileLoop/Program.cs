29079f5c75047d2844c4267edfa6411f66d89f52
madhunepal <madhunepal@gmail.com>
2018-02-24 00:09:23 -0500

    do while loop conditions are added

---
+++ doWhileLoop/doWhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wildTurkey = 0;
+            do
+            {
+                Console.WriteLine("wildturkey = {wildturkey}");
+                wildTurkey++;
+            }
+            while (wildTurkey < 1);
+            
+        }
+    }
+}

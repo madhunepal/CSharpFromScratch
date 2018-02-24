13adb66679fb02450c941581b07de01c10f318b4
madhunepal <madhunepal@gmail.com>
2018-02-24 00:28:31 -0500

    switch statment in simple way

---
+++ switchStatement/switchStatement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchStatement
+{
+    class Program
+    {
+        enum Movies
+        {
+            Thuli,
+            Kri,
+            Baagh,
+        }
+        static void Main(string[] args)
+        {
+            Movies bestMovie = Movies.Baagh;
+            switch (bestMovie)
+            {
+                case Movies.Thuli:
+                    Console.WriteLine("This is women empoerment movie");
+                    break;
+                case Movies.Baagh:
+                    Console.WriteLine("I will make him an offer....");
+                    break;
+                case Movies.Kri:
+                    Console.WriteLine("This is flop movie");
+                    break;
+                default:
+                    Console.WriteLine("you must make an choice");
+                    break;
+
+            }
+        }
+    }
+}

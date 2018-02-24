5bf5c3d625a5a74c69219b7ac4e56002db1f235f
madhunepal <madhunepal@gmail.com>
2018-02-23 23:57:29 -0500

    while loop demo

---
+++ whileLoopDemo2/whileLoopDemo2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoopDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iHaveGoats = 0;
+            while (iHaveGoats < 10)
+            {
+                Console.WriteLine("ihavegoates = {ihavegoates}");
+                iHaveGoats++;
+            }
+        }
+    }
+}

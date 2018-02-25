d13c156124ba006ab4c8deca8e7c42b4b4f63721
madhunepal <madhunepal@gmail.com>
2018-02-25 18:05:19 -0500

    passing one variable using readline

---
+++ JustCode/JustCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JustCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello," + name);
+        }
+    }
+}

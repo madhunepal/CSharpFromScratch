8967f9c1428fd7182ee4bd2440a5bc4ed232ad90
madhunepal <madhunepal@gmail.com>
2018-02-24 22:52:43 -0500

    calculation of odd number is added

---
+++ OddEven/OddEven/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddEven {
+    class Calculation
+    {
+        static void Main()
+        {
+
+            // Print even numbers between 0 and 100.
+            for (int i = 0; i <= 100; i++)
+            {
+                if ((i % 2) != 0)
+                    continue; // iterate
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

bd6589520ad116b44d92dfb5d8a0af0d0b2e933f
madhunepal <madhunepal@gmail.com>
2018-03-18 12:36:41 -0400

    latest commit

--- CSharpFromScratch/SimpleProgram.cs
+++ CSharpFromScratch/SimpleProgram.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpFromScratch
 {
@@ -11,7 +7,7 @@ namespace CSharpFromScratch
         static void main(string[] arg)
         {
             int myAge = 32;
-            string lastName = "Nepal";
+  
             string firstName = "madhu";
             double myPayRate = 56.00;
 
